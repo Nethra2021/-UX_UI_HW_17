59a2a191cd7bf96ae2abeb566f00cb2b4e567ccd
Nethra2021 <85814168+Nethra2021@users.noreply.github.com>
2021-11-18 10:25:46 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,4 +3,12 @@ $("li a").click(function(){
   var pageId = $(this).attr("data-page");
   $("html, body").animate({ scrollTop: $("#"+pageId).offset().top - 150}, 1000);
 });
+
+function hover(element) {
+  element.setAttribute('src', 'images/Project1Hover.png');
+}
+
+function unhover(element) {
+  element.setAttribute('src', 'images/Project1.png"');
+}
 });
